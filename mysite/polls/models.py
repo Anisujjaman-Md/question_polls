29cd19c87deed1b4244cb86c8dd4c94603c1d21e
Anis <Contact.anisujjaman@gmail.com>
2022-08-10 13:58:56 +0600

    Question and Polls

---
+++ mysite/polls/models.py
@@ -0,0 +1,23 @@
+from secrets import choice
+from django.utils import timezone
+from django.db import models
+import datetime as dt
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    publication_date = models.DateTimeField('Date Publised')
+    
+    def was_published_recently(self):
+        return self.publication_date >= timezone.now() - dt.timedelta(days = 1)
+    
+    def __str__(self):
+        return self.question_text
+    
+class Choice(models.Model):
+    questions = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.choice_text
