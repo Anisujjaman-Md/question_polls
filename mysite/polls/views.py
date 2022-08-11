3dc5866f592de3abc5950794286dd906fd4ce58a
Anis <contact.anisujjaman@gmail.com>
2022-08-11 17:59:28 +0600

    11-08-2022

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,9 @@
 from re import template
-from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
-
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 
 """Create your views here."""
@@ -27,9 +27,17 @@ def index(request):
     context = {'latest_question_list': latest_question_list}   
     return render(request,'polls/index.html', context)
 
+# def detail(request, question_id): 
+#     try: 
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question Does not Found")
+#     return render(request, 'polls/details.html', {'question' : question})
 
-def detail(request, question_id):  
-    return HttpResponse("You're looking at question %s, and current tim is: " %question_id )
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/details.html', {'question' : question})
+          
 
 def result(request, question_id):
     response = "You're looking at the results of question %s"
