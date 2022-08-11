ea088550e08c26477211c11f7ac580f1eac3853c
Anis <Contact.anisujjaman@gmail.com>
2022-08-11 10:16:35 +0600

    Test

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
 
-    path('<int:question_id>/', views.detail, name='detail'),
+urlpatterns = [
+    path('<str:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/result/', views.result, name = 'result'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,8 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." %question_id)
+def detail(request, question_id, time):
+    """
+        Next Days Work 11/08/2022
+    """
+    time = "abc"
+    return HttpResponse("You're looking at question %s, and current tim is: %s " %question_id %time )
 
 def result(request, question_id):
     response = "You're looking at the results of question %s"
@@ -12,4 +16,3 @@ def result(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s" %question_id)
-
