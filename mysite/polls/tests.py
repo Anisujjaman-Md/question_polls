b01419f7d8312a327c7ab9a5a8f643eb4fab0b4c
Anis <contact.anisujjaman@gmail.com>
2022-08-17 11:06:38 +0600

    Test

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,16 @@
+from asyncio import futures
 from django.test import TestCase
 
 # Create your tests here.
+
+import datetime
+from django.utils import timezone
+from .models import Question
+
+class QuestionModelTest():
+    def test_was_published_recently_with_future_question(self):
+        """Test_was_publised_recently_with_future_question() return false pr question whose publication_date
+        is in the future"""
+        time = timezone.now() + datetime.timedelta(days = 30)
+        future_question = Question(publication_date = time)
+        self.assertIs(future_question.was_published_recently(),False)
